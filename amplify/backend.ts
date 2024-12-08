9ca9e548de3fd45904927f6e8b1f5920bd922d6f
Sedivyk <sedivyk@gmail.com>
2024-12-08 14:16:53 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -8,7 +8,7 @@ import {
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
 import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { myApiFunction } from "./functions/api-functions/resource";
+import { myApiFunction } from "./functions/api-functions/resources";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 
