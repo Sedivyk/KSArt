91a7ce9a7ad9ced8d0f5582a66a72c915feec4ee
Sedivyk <sedivyk@gmail.com>
2024-12-08 15:09:20 -0500

    Update resources.ts

--- amplify/functions/api-functions/resources.ts
+++ amplify/functions/api-functions/resources.ts
@@ -3,7 +3,3 @@ import { defineFunction } from "@aws-amplify/backend";
 export const myApiFunction = defineFunction({
   name: "api-functions",
 });
-
-export const ksSendEventFunction = defineFunction({
-  name: "ks-send-event-function",
-});
